63fa061dd9e20a27155a63f9f1a562560c4bab27
joseiltonjunior <joseiltonjuniortech@gmail.com>
2020-10-12 19:24:32 -0300

    finish screen landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,8 @@
 import React from "react";
-import { FiArrowRight } from "react-icons/fi";
 
 import "./styles/global.css";
-import "./styles/pages/landing.css";
-import logoImg from "./assets/icons/Logo.svg";
+import Routes from "./routes";
 
 export default function App() {
-  return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Happy" />
-
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>
-            Visite orfanatos, doe brinquedos e mude o dia de muitas crianças.
-          </p>
-        </main>
-
-        <div className="location">
-          <strong>Recife</strong>
-          <span>Pernambuco</span>
-        </div>
-        <a href="" className="enter-app">
-          <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
-        </a>
-      </div>
-    </div>
-  );
+  return <Routes />;
 }
---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function OrphanagesMap() {
+  return <h1>Mapa</h1>;
+}
