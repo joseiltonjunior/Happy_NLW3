6f52ba7670517caaa697aff2c06c5153f0a40d6d
joseiltonjunior <joseiltonjuniortech@gmail.com>
2020-10-12 13:39:26 -0300

    implementation landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
 import React from "react";
+import { FiArrowRight } from "react-icons/fi";
 
-function App() {
+import "./styles/global.css";
+import "./styles/pages/landing.css";
+import logoImg from "./assets/icons/Logo.svg";
+
+export default function App() {
   return (
-    <div className="App">
-      <h1>Happy</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>
+            Visite orfanatos, doe brinquedos e mude o dia de muitas crianças.
+          </p>
+        </main>
+
+        <div className="location">
+          <strong>Recife</strong>
+          <span>Pernambuco</span>
+        </div>
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0,0,0,0.6)" />
+        </a>
+      </div>
     </div>
   );
 }
-
-export default App;
